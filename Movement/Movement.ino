b70bed861b8146edd946d38be64854a12ddda093
Hardik Khurana <hardik0330@gmail.com>
2019-07-29 23:19:11 +0530

    motion achieved

--- Movement/Movement.ino
+++ Movement/Movement.ino
@@ -12,15 +12,29 @@ void setup() {
   pwm.setPWMFreq(60);  
   //delay(10);
 }
+void lto()
+{
+  pwm.setPWM(5, 0, 300);
+  pwm.setPWM(1, 0, 300);
+  pwm.setPWM(7, 0, 300);
+  pwm.setPWM(2, 0, 300);
 
-void loop() {
-  pwm.setPWM(5, 0, SERVOMAX);
-  pwm.setPWM(1, 0, SERVOMAX);
-  pwm.setPWM(7, 0, SERVOMAX);
-  pwm.setPWM(2, 0, SERVOMAX);
-
-  pwm.setPWM(4, 0, SERVOMIN);
-  pwm.setPWM(0, 0, SERVOMIN);
-  pwm.setPWM(6, 0, SERVOMIN);
+  pwm.setPWM(4, 0, SERVOMAX);
+  pwm.setPWM(0, 0, SERVOMAX);
+  pwm.setPWM(6, 0, SERVOMAX);
+  pwm.setPWM(3, 0, SERVOMAX);
+  delay(1000);
   pwm.setPWM(3, 0, SERVOMIN);
+  delay(200);
+  pwm.setPWM(5, 0, SERVOMIN);
+  delay(200);
+//  pwm.setPWM(3, 0, SERVOMAX);
+//  delay(200);
+//  pwm.setPWM(5, 0, 450);
+//  pwm.setPWM(1, 0, SERVOMIN);
+//  pwm.setPWM(7, 0, SERVOMIN);
+  delay(10000);
+}
+void loop() {
+  lto();
 }
